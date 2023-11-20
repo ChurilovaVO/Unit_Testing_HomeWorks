6052b2bafab8e67fd6379182ecf7ce521230b09e
Valentina <vansimova@yandex.ru>
2023-11-20 19:22:49 +0300

    TestMethod added

--- HomeWork__3/src/main/java/hw/Method.java
+++ HomeWork__3/src/main/java/hw/Method.java
@@ -1,2 +1,23 @@
-package hw;public class Method {
+package hw;
+
+public class Method {
+
+    //Задание 1. Метод проверяет, является ли переданное число четным или нечетным
+    public boolean evenOddNumber(int n) {
+        if (n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Задание 2. Метод numberInInterval, который проверяет, попадает ли
+    //переданное число в интервал (25;100)
+
+    public boolean numberInInterval(int n) {
+        if (n >= 25 && n<=100) {
+            return true;
+        }
+        return false;
+    }
 }
--- HomeWork__3/src/test/java/TestMethods.java
+++ HomeWork__3/src/test/java/TestMethods.java
@@ -1,13 +1,49 @@
+import hw.Method;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class Test {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMethods {
+    Method method;
+
+    @BeforeEach
+    void setUp() {
+        method = new Method();
+    }
 
     //Задание 1. Напишите тесты, покрывающие на 100% метод evenOddNumber, который проверяет, является ли
     //переданное число четным или нечетным
 
-
-    @Test
-    public void testEvenOddNumber(){
-
+    @ParameterizedTest
+    @ValueSource(ints = {-4, 0, 6})
+    void testEvenOddNumberEven(int i) {
+        assertTrue(method.evenOddNumber(i));
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-5, 11})
+    void testEvenOddNumberOdd(int i) {
+        assertFalse(method.evenOddNumber(i));
+    }
+
+    //Задание 2. Разработайте и протестируйте метод numberInInterval, который проверяет, попадает ли
+    //переданное число в интервал (25;100)
+
+    @Test
+    void testNumberInIntervalIn(){
+        assertTrue(method.numberInInterval(50));
+    }
+    @Test
+    void testNumberInIntervalOut(){
+        assertFalse(method.numberInInterval(20));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {25,100})
+    void testNumberInIntervalEdge(int i) {
+        assertTrue(method.numberInInterval(i));
+    }
 }
